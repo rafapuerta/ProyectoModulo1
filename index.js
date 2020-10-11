41276e454ea2652b45eb27415630e9540d76160f
rafapuerta <info@rafapuerta.com>
2020-10-11 13:36:45 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const url = "http://www.omdbapi.com/?apikey=22e38317";
+let busqueda = ""
+let newUrl = ""
+let opciones = ""
+/* Declaración de variables */
+
+/* Funciones */
+
+function searchTitle() {
+  busqueda = document.getElementById("busqueda").value;
+  console.log(busqueda);
+  newUrl = url + "?s=" + busqueda
+  console.log(newUrl)
+  fetch(newUrl)
+    .then(function (respuesta) {
+      return respuesta.json();
+    })
+    .then(function (datos) {
+      for (let i = 0; i < datos.results.length; i++) {
+        opciones += `<p>${datos.results[i].title}</p>`;
+      }
+      document.getElementById("div").innerHTML = opciones;
+    });
+}
