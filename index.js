47a3406abad3a767ad0d1815a0e3f1971852414e
rafapuerta <info@rafapuerta.com>
2020-10-13 00:23:39 +0200

    fix

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ let poster = "";
 let resultado = "";
 let listaFavoritos = [];
 let listaTemporal = []
+let favoritos = ""
 /*----------------------------------------- Declaración de variables -----------------------------------------*/
+
+actualizaFavoritos()
 
 /*----------------------------------------- Funciones -----------------------------------------*/
 
@@ -34,10 +37,10 @@ function search() {
           datos.Search[i].Poster
         }" style="width: 100%" ; />
         <div class="container">
-          <h2 id="titulo"><a onclick="peliID(${
+          <h3 id="titulo"><a onclick="peliID(${
             datos.Search[i].imdbID
-          })" href="">${datos.Search[i].Title}</a></h2>
-          <h4 id="anno">${datos.Search[i].Year}</h4>
+          })" href="">${datos.Search[i].Title}</a></h3>
+          <h5 id="anno">${datos.Search[i].Year}</h5>
         </div>
         <div id="favorito">
           <i onclick="hacerFavorito(${i})" id="favorito" class="material-icons">${esFavorito(datos.Search[i].imdbID)}</i>
@@ -45,7 +48,6 @@ function search() {
       </div>
       `;
         }
-        console.log( typeof datos.Search[0].imdbID )
         document.getElementById("div2").innerHTML = opciones;
       }
     });
@@ -106,19 +108,64 @@ function letrasRandom(tamano) {
 
 
 
-
+function actualizaFavoritos (){
+  if (localStorage.getItem('listaFavoritos') == null){
+    localStorage.setItem("listaFavoritos", JSON.stringify(listaFavoritos))
+  }
+  listaFavoritos = JSON.parse(localStorage.getItem('listaFavoritos'))
+}
 
 function esFavorito(id) {
-  for (let i = 0; i <= listaFavoritos.length; i++) {
-    if (listaFavoritos[i] == id) {
+  if (listaFavoritos.includes(id)){
       return "favorite";
     } else {
       return "favorite_border";
     }
-  }
 }
 
 function hacerFavorito(id) {
-  listaFavoritos.push(listaTemporal[id]);
-  localStorage.setItem("listaFavoritos", JSON.stringify(listaFavoritos));
+    console.log(listaFavoritos)
+    console.log(listaTemporal[id])
+    if ( !listaFavoritos.includes(listaTemporal[id])){
+      listaFavoritos.push(listaTemporal[id]);
+      localStorage.setItem("listaFavoritos", JSON.stringify(listaFavoritos));
+      console.log(listaFavoritos)
+    }else{
+      listaFavoritos.splice(listaFavoritos.indexOf(listaTemporal[id]),listaFavoritos.indexOf(listaTemporal[id])+1)
+      localStorage.setItem("listaFavoritos", JSON.stringify(listaFavoritos));
+      console.log(listaFavoritos)
+    }
+}
+
+function ensenaFavoritos(){
+  console.log(listaFavoritos)
+  actualizaFavoritos();
+  if (listaFavoritos[0] !== 0){
+    for (let i = 0 ; i< listaFavoritos.length ; i++){
+      fetch(url + "&i=" + listaFavoritos[i])
+        .then(function (respuesta) {
+          return respuesta.json();
+        })
+        .then(function (datos) {
+        favoritos += `<div id="peliFavorita">
+          <div id="peliFavoritaPoster">
+            <img src="${datos.Poster}" alt="${datos.Title}"/>
+          </div>
+          <div id="peliFavoritaDatos">
+            <h3>${datos.Title}</h3>
+            <h4>${datos.Year}</h4>
+            <h6><strong> Duración:</strong> ${datos.Runtime}</h6>
+            <h6><strong> Director:</strong>  ${datos.Director}</h6>
+            <h6><strong> Premios:</strong>  ${datos.Awards}</h6>
+            <h6><strong> Puntuancion:</strong>  Rotten Tomatoes${datos.Ratings[2].Value}</h6>
+            <p>${datos.Plot}</p>
+          </div>
+        </div>`
+        document.getElementById("divFavoritos").innerHTML = favoritos
+    })
+  };
+  }else{
+    document.getElementById("divFavoritos").innerHTML = "<h1>Oops!</h1><h4>No tienes favoritos guardados</h4>"
+  }
+  
 }
