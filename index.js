d9e23f7207374b307e548a79f980b21a9926d656
rafapuerta <info@rafapuerta.com>
2020-10-15 21:27:22 +0200

    Popup System

--- index.js
+++ index.js
@@ -39,9 +39,7 @@ function search() {
           datos.Search[i].Poster
         }" style="width: 100%" ; />
         <div class="container">
-          <h3 id="titulo"><a onclick="peliID(${
-            datos.Search[i].imdbID
-          })" href="">${datos.Search[i].Title}</a></h3>
+          <h3 id="titulo"><a onclick="peliID(${i})">${datos.Search[i].Title}</a></h3>
           <h5 id="anno">${datos.Search[i].Year}</h5>
         </div>
         <div id="favorito">
@@ -55,19 +53,32 @@ function search() {
     });
 }
 
-/* function peliID(id) {
-  fetch(url + "&i=" + id)
+function peliID(id) {
+  console.log(listaTemporal[id])
+  abrePop();
+  fetch(url + "&i=" + listaTemporal[id])
     .then(function (respuesta) {
       return respuesta.json();
     })
     .then(function (datos) {
-      console.log(url + "&i=" + id);
-      document.getElementById("div2").innerHTML = `
-      <h1>${datos.Title}</h1>
-      <img src="${datos.Poster}" alt="${datos.Title}" />
-      <h3>${datos.Director}</h3>`;
+      document.getElementById("infoPeli").innerHTML = 
+      `<div id="peliFavorita">
+      <div id="peliFavoritaPoster2">
+        <img src="${datos.Poster}" alt="${datos.Title}"/>
+      </div>
+      <div id="peliFavoritaDatos">
+        <h3>${datos.Title}</h3>
+        <h4>${datos.Year}</h4>
+        <h6><strong> Duración:</strong> ${datos.Runtime}</h6>
+        <h6><strong> Director:</strong>  ${datos.Director}</h6>
+        <h6><strong> Premios:</strong>  ${datos.Awards}</h6>
+        <h6><strong> Puntuancion:</strong>  Rotten Tomatoes: ${datos.Ratings[1].Value}</h6>
+        <p>${datos.Plot}</p>
+        
+      </div>
+    </div>`;
     });
-} */
+}
 
 function finalUrl(string, anyo, tipo) {
   if (anyo !== "") {
@@ -172,4 +183,12 @@ function ensenaFavoritos(){
     document.getElementById("divFavoritos").innerHTML = "<h1>Oops!</h1><h4>No tienes favoritos guardados</h4>"
   }
   
+}
+
+function abrePop() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function cierraPop() {
+  document.getElementById("popup").style.display = "none";
 }
